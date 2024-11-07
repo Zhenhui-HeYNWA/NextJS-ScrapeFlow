96d92296da803109cd3b610111f6cc7a32e7db2a
Zhenhui-HeYNWA <herbertynwa@outlook.com>
2024-11-07 22:18:39 +1100

    add CreateWorkflow and deleteWorkflow dialogs

---
+++ scrape-flow/app/(dashboard)/workflows/page.tsx
@@ -0,0 +1,76 @@
+import { GetWorkflowsForUser } from "@/actions/workflows/getWorkflowsForUser";
+import { Skeleton } from "@/components/ui/skeleton";
+
+import React, { Suspense } from "react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertCircle, InboxIcon } from "lucide-react";
+import CreateWorkflowDialog from "./_components/CreateWorkflowDialog";
+import WorkflowCard from "./_components/WorkflowCard";
+
+function page() {
+  return (
+    <div className="flex h-full flex-1 flex-col">
+      <div className="flex justify-between">
+        <div className="flex flex-col">
+          <h1 className="text-3xl font-bold">Workflows</h1>
+          <p className="text-muted-foreground">Manage your Workflows</p>
+        </div>
+        <CreateWorkflowDialog />
+      </div>
+
+      <div className="h-full py-6">
+        <Suspense fallback={<UserWorkflowsSkeleton />}>
+          <UserWorkflows />
+        </Suspense>
+      </div>
+    </div>
+  );
+}
+function UserWorkflowsSkeleton() {
+  return (
+    <div className="space-y-2">
+      {[1, 2, 3, 4].map((i) => (
+        <Skeleton key={i} className="h-32 w-full" />
+      ))}
+    </div>
+  );
+}
+
+async function UserWorkflows() {
+  const workflows = await GetWorkflowsForUser();
+  if (!workflows) {
+    return (
+      <Alert variant={"destructive"}>
+        <AlertCircle className="h-4 w-4" />
+        <AlertTitle>Error</AlertTitle>
+        <AlertDescription>
+          Something went wrong.Please try again later
+        </AlertDescription>
+      </Alert>
+    );
+  }
+  if (workflows.length === 0) {
+    return (
+      <div className="flex h-full flex-col items-center justify-center gap-4">
+        <div className="flex h-20 w-20 items-center justify-center rounded-full bg-accent">
+          <InboxIcon size={40} className="stroke-primary" />
+        </div>
+        <div className="flex flex-col gap-1 text-center">
+          <p className="font-bold">No workflow created yet</p>
+          <p className="text-sm text-muted-foreground">
+            Click the button below to create your first workflow
+          </p>
+        </div>
+        <CreateWorkflowDialog triggerText="Create your first workflow" />
+      </div>
+    );
+  }
+  return (
+    <div className="grid grid-cols-1 gap-4">
+      {workflows.map((workflow) => (
+        <WorkflowCard key={workflow.id} workflow={workflow} />
+      ))}
+    </div>
+  );
+}
+export default page;
